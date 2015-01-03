171924e117119baad09ed8a6e6c168755224d20d
ThibmoRozier <thibmorozier@gmail.com>
2015-01-03 13:12:18 +0100

    Added a timeout to the web class.

--- PyRadmon/PyRadmon.py
+++ PyRadmon/PyRadmon.py
@@ -536,8 +536,8 @@ class audioCommunication(threading.Thread):
 # Part 3 - Web server communication
 ################################################################################
 class webCommunication():
-    HOST="www.radmon.org"
-    #HOST="127.0.0.1" # uncomment this for debug purposes on localhost
+    #HOST="www.radmon.org"
+    HOST="127.0.0.1" # uncomment this for debug purposes on localhost
     PORT=80
 
     def __init__(self, mycfg):
@@ -545,10 +545,10 @@ class webCommunication():
         self.password=mycfg.password
 
     def sendSample(self, sample):
-
         print "Connecting to server => geiger 1\r\n"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.HOST, self.PORT))
+        s.settimeout(10.0)
 
         BUFFER_SIZE=1024
 
@@ -564,15 +564,26 @@ class webCommunication():
         print "Sending average sample => geiger 1: "+str(sampleCPM)+" CPM\r\n"
         #print "\r\n### HTTP Request ###\r\n"+request
 
-        s.send(request)
-        data = s.recv(BUFFER_SIZE)
-        httpResponse=str(data).splitlines()[0]
-        print "Server response => geiger 1: ",httpResponse,"\r\n"
+        try:
+            data = None
+            doneSend = False
+            s.send(request)
+            time.sleep(0.5)
+            data = s.recv(BUFFER_SIZE)
+            time.sleep(0.5)
+            for i in range(10):
+                if doneSend is False:
+                    if data is not None:
+                        httpResponse=str(data).splitlines()[0]
+                        print "Server response => geiger 1: ",httpResponse,"\r\n"
 
-        if "incorrect login" in data.lower():
-            print "You are using incorrect user/password combination => geiger 1!\r\n"
-            geigerCommunication.stop()
-            sys.exit(1)
+                        if "incorrect login" in data.lower():
+                            print "You are using incorrect user/password combination => geiger 1!\r\n"
+                            geigerCommunication.stop()
+                            sys.exit(1)
+                        doneSend = True
+        except Exception as ex:
+            print "Could not communicate with the Server, timeout reached. => geiger 1: ",ex,"\r\n"
             
         #print "\r\n### HTTP Response ###\r\n"+data+"\r\n"
         s.close
@@ -609,9 +620,9 @@ if (os.path.isfile("config.txt")==0):
         f.write("device=0\r\n")
         f.close()
         print "\tPlease open config.txt file using text editor and update configuration.\r\n"
-        exit(1)
     except Exception as e:
         print "\tFailed to create configuration file\r\n\t",str(e)
+    time.sleep(1)
     exit(1)
 else:
     # create and read configuration data
